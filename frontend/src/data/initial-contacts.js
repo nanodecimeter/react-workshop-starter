58270aad99539e388af37d83bc7df6c00401ac50
Andrew Meads <andrew.meads@auckland.ac.nz>
2025-05-02 14:33:54 +1200

    Updated starting point to 2025 version

--- frontend/src/data/initial-contacts.js
+++ frontend/src/data/initial-contacts.js
@@ -4,48 +4,48 @@ export const INITIAL_CONTACTS = [
     name: "Luke Skywalker",
     phoneNumber: "+1 555-000-1001",
     funFact: "Has a green lightsaber in 'Return of the Jedi'.",
-    photoUrl: "https://th.bing.com/th/id/OIP.PoS7waY4-VeqgNuBSxVUogAAAA?rs=1&pid=ImgDetMain"
+    photoUrl: "https://randomuser.me/api/portraits/men/44.jpg"
   },
   {
     _id: "5f90d372-7bdf-4b39-b621-cf6d3cd975f4",
     name: "Hermione Granger",
     phoneNumber: "+1 555-000-1002",
     funFact: "Uses the Time-Turner in 'The Prisoner of Azkaban'.",
-    photoUrl: "https://th.bing.com/th/id/OIP.PoS7waY4-VeqgNuBSxVUogAAAA?rs=1&pid=ImgDetMain"
+    photoUrl: "https://randomuser.me/api/portraits/women/38.jpg"
   },
   {
-    _id: "b8853e3f-38d1-4712-9071-8f62d71c19b8",
-    name: "Walter White",
-    phoneNumber: "+1 555-000-1003",
-    funFact: "Is also known as Heisenberg.",
-    photoUrl: "https://th.bing.com/th/id/OIP.PoS7waY4-VeqgNuBSxVUogAAAA?rs=1&pid=ImgDetMain"
+    _id: "3b62658c-3c23-4bfc-812b-36b0c6076dd8",
+    name: "Katniss Everdeen",
+    phoneNumber: "+1 555-000-1019",
+    funFact: "Is known as the 'Girl on Fire'.",
+    photoUrl: "https://randomuser.me/api/portraits/women/83.jpg"
   },
   {
     _id: "18fb9676-3c5b-4480-b8bc-4b9cc2f6ef28",
     name: "Darth Vader",
     phoneNumber: "+1 555-000-1004",
     funFact: "Was originally a Jedi named Anakin Skywalker.",
-    photoUrl: "https://th.bing.com/th/id/OIP.PoS7waY4-VeqgNuBSxVUogAAAA?rs=1&pid=ImgDetMain"
+    photoUrl: "https://randomuser.me/api/portraits/men/35.jpg"
   },
   {
     _id: "983c620f-6500-476f-a856-8791aac32794",
     name: "Tony Stark",
     phoneNumber: "+1 555-000-1005",
     funFact: "Is the real name of Iron Man.",
-    photoUrl: "https://th.bing.com/th/id/OIP.PoS7waY4-VeqgNuBSxVUogAAAA?rs=1&pid=ImgDetMain"
+    photoUrl: "https://randomuser.me/api/portraits/men/68.jpg"
   },
   {
-    _id: "99fb1c56-98b3-41a9-b67b-580567495e86",
-    name: "Sherlock Holmes",
-    phoneNumber: "+1 555-000-1006",
-    funFact: "Plays the violin when he is thinking.",
-    photoUrl: "https://th.bing.com/th/id/OIP.PoS7waY4-VeqgNuBSxVUogAAAA?rs=1&pid=ImgDetMain"
+    _id: "0e37df36-f698-41e4-8270-3b5ded18a4ff",
+    name: "Ellen Ripley",
+    phoneNumber: "+1 555-000-1012",
+    funFact: "Is the main character in the movie 'Alien'.",
+    photoUrl: "https://randomuser.me/api/portraits/women/89.jpg"
   },
   {
     _id: "71b5ed07-9e1e-47d0-937a-64c396d5567b",
     name: "Bruce Wayne",
     phoneNumber: "+1 555-000-1007",
     funFact: "Lives in Gotham City.",
-    photoUrl: "https://th.bing.com/th/id/OIP.PoS7waY4-VeqgNuBSxVUogAAAA?rs=1&pid=ImgDetMain"
+    photoUrl: "https://randomuser.me/api/portraits/men/91.jpg"
   }
 ];
