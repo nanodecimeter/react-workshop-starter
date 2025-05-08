25ec93b3dc4a94fecf8df383b5a603a1436dedb0
Andrew Meads <andrew.meads@auckland.ac.nz>
2025-05-08 16:38:33 +1200

    Removed unimportant functions from API file

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,25 +1,6 @@
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 /**
- * Sends a POST request to the server to create a new contact.
- *
- * @param {*} contact the new contact
- *
- * @returns a promise which will either resolve to the new contact object returned by the server,
- *          or will reject with the response object if a response outside the 200-299 range is returned.
- */
-export async function createContact(contact) {
-  const response = await fetch(BASE_URL, {
-    method: "POST",
-    body: JSON.stringify(contact),
-    headers: { "content-type": "application/json" }
-  });
-
-  if (response.ok) return await response.json();
-  throw { response };
-}
-
-/**
  * Sends a GET request to the server to get all contacts.
  *
  * @returns a promise which will resolve to an array of all contact info on the server.
@@ -27,37 +8,4 @@ export async function createContact(contact) {
 export async function retrieveContacts() {
   const response = await fetch(BASE_URL);
   return await response.json();
-}
-
-/**
- * Sends a PATCH request to the server to update a contact.
- *
- * @param {*} contact the contact to update
- *
- * @returns a promise which will either resolve if the update was successful, or reject with the
- *          response object if a response object outside the 200-299 range is returned.
- */
-export async function updateContact(contact) {
-  console.log("apiUpdateContact", contact);
-  const response = await fetch(`${BASE_URL}/${contact._id}`, {
-    method: "PATCH",
-    body: JSON.stringify(contact),
-    headers: { "content-type": "application/json" }
-  });
-  if (!response.ok) throw { response };
-}
-
-/**
- * Sends a DELETE request to the server to delete a contact.
- *
- * @param {string} id the id of the contact to delete
- *
- * @returns a promise which will either resolve if the deletion was successful, or reject with the
- *          response object if a response object outside the 200-299 range is returned.
- */
-export async function deleteContact(id) {
-  const response = await fetch(`${BASE_URL}/${id}`, {
-    method: "DELETE"
-  });
-  if (!response.ok) throw { response };
 }
