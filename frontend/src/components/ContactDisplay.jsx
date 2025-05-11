d6980ad1edabf9a12e89968b07fdf3f358b8648e
Abdul-Qudus Amidu <aami800@aucklanduni.ac.nz>
2025-05-11 12:25:23 +1200

    Another alternative way
    Object Destructuring

--- frontend/src/components/ContactDisplay.jsx
+++ frontend/src/components/ContactDisplay.jsx
@@ -2,13 +2,15 @@ import { getPhotoUrl } from "../util/image-utils";
 
 export default function ContactDisplay({contact}){
 
+    const {name, phoneNumber, funFact, photoUrl} = contact;
+
     return(
         <>
         <section className="contact-display">
-            <img src = {getPhotoUrl(contact.photoUrl)}/>
-            <h1>{contact.name}</h1>
-            <h3>{contact.phoneNumber}</h3>
-            <p>Fun Fact: {contact.funFact}</p>
+            <img src = {getPhotoUrl(photoUrl)}/>
+            <h1>{name}</h1>
+            <h3>{phoneNumber}</h3>
+            <p>Fun Fact: {funFact}</p>
         </section>
         </>
     )
